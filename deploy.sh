fa29ffa33ba20c8c7b946938af728b3cbf1e532b
Amir Hossein Goodarzi <amir@goodarzi.net>
2019-11-09 16:21:13 +0330

    fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ then
     chmod 0600 ~/.git-credentials
     echo $GITHUB_AUTH_SECRET > ~/.git-credentials
     git config credential.helper store
-    git config user.email "username-blog-bot@users.noreply.github.com"
-    git config user.name "username-blog-bot"
+    git config user.email "khatefarman-deploy-bot@users.noreply.github.com"
+    git config user.name "khatefarman-deploy-bot"
 fi
 git add .
 git commit -m "Rebuild site"
-git push --force origin HEAD:mastera
+git push --force origin HEAD:master
