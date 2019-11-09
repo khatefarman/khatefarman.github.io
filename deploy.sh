8edf6ef885a8f64511dcddc04882f06626a55812
Amir Hossein Goodarzi <amir@goodarzi.net>
2019-11-09 17:16:39 +0330

    fix

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,6 @@ hugo
 # deploy
 cd public
 git init
-git checkout -b gh-pages
 git add .
 git commit -m "Deploy to Github Pages"
 git push --force --set-upstream "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages
