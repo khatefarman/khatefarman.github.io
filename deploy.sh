4cedf5ae3b115024cf37f7a57191feb4a67a5479
Amir Hossein Goodarzi <amir@goodarzi.net>
2019-11-09 17:20:21 +0330

    fix

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ rm -rf public
 mkdir public
 
 # config
-git config --global user.email "nobody@nobody.org"
-git config --global user.name "Travis CI"
+git config --global user.email "khatefarman-deploy-bot@users.noreply.github.com"
+git config --global user.name "khatefarman-deploy-bot"
 
 # build (CHANGE THIS)
 hugo
@@ -16,4 +16,4 @@ cd public
 git init
 git add .
 git commit -m "Deploy to Github Pages"
-git push --force --set-upstream "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages
+git push --force --set-upstream "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master
